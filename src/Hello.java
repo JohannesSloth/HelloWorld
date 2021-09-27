d9c20a85215349c98db2d01070165962c7249028
JohannesSloth <joha218s@stud.kea.dk>
2021-09-27 15:27:27 +0200

    First commit

---
+++ src/Hello.java
@@ -0,0 +1,9 @@
+public class Hello {
+    public static void main(String[] args) {
+        //Generate some simple output
+        System.out.println("Hello, World!");
+        System.out.println("What's up?\nLet's go");
+        System.out.print("Hvad såååå");
+        System.out.print("neeeej");
+    }
+}
